3e7b1c6186d4ecb5fbae6c997a80f8337325b757
Shut <a.shut@shuta-l.itransition.corp>
2013-04-24 16:16:15 +0300

    ckeditor saving data, admin role

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Templates::Application.config.secret_token = '9c122fb1ce1b345e10ece579ce23f6c7af88644b40830ca8ed1d7fe3c1a8130c23d079066f18ba8685c6c2babaf252530060d4529fa02e697a53174c398544c0'
